e3f688f4cec5c8dce0e19d918b23da8b5f040191
Morski1337 <26685032+Dungeon-Morski@users.noreply.github.com>
2022-07-01 00:09:19 +0300

    Add files via upload

--- faq/main.js
+++ faq/main.js
@@ -1,75 +1,90 @@
-/*onepagescroll('.pages',{
-    pageContainer: 'section',     
-    animationType: 'ease-in-out', 
-    animationTime: 700,        
-    infinite: false,           
-    pagination: false,             
-    keyboard: false,               
-    direction: 'vertical'        
-     
-  });
-  let wasSmall = false; 
-  let wasBig = false;
-  
-  const resizeObserver = new ResizeObserver(entries => {
-    if(entries[0].target.clientHeight < 800 && !wasSmall && entries[0].target.clientHeight < 1000){
-        wasSmall = true;
-        wasBig = false
-        console.log("hss")
-		window.removeEventListener('wheel',onScrollEventHandler);
-        document.body.setAttribute("style","overflow:scroll!important;overflow-x: hidden !important;")
-        document.querySelector(".pages").setAttribute("style","transform: translate3d(0px, 0px, 0px);")
-    }
-   
-}
-  )
-  
-  resizeObserver.observe(document.body)
-  */
 
-document.body.classList.add("scroll");
-const modalcontainer = document.querySelector(".modalcontainer");
-document.getElementById("openmodal").onclick = () => {
+        document.body.classList.add("scroll")
+  const modalcontainer = document.querySelector(".modalcontainer")
+  document.getElementById("openmodal").addEventListener("click", ()=>{
+    modalcontainer.classList.toggle("visible")
+  })
+  document.getElementById("openmodal").addEventListener("touchend", ()=>{
+    modalcontainer.classList.toggle("visible")
+  })
+modalcontainer.querySelector(".modalwall").addEventListener("click", () => {
   modalcontainer.classList.toggle("visible");
-};
-modalcontainer.querySelector(".modalwall").onclick = () => {
-  modalcontainer.classList.toggle("visible");
-};
+})
 
-modalcontainer.querySelector(".cross").onclick = () => {
+modalcontainer.querySelector(".modalwall").addEventListener("touchend", () => {
   modalcontainer.classList.toggle("visible");
-};
-const menu = document.querySelector(".mobilemenu > div");
-menu.addEventListener("click", () => {
-  document.querySelector(".pages").classList.toggle("menuactive");
-  document.body.classList.toggle("menuactive");
-  document
-    .querySelector(".pages")
-    .setAttribute(
-      "style",
-      "transition: transform 700ms ease-in-out 0s;transform:none!important;"
-    );
+})
+  modalcontainer.querySelector(".cross").addEventListener("click",()=>{
+
+    modalcontainer.classList.toggle("visible")
+  })
+  modalcontainer.querySelector(".cross").addEventListener("touchend",()=>{
+
+    modalcontainer.classList.toggle("visible")
+  })
+const menu = document.querySelector('.mobilemenu > div');
+menu.addEventListener('click', () => {
+  
+  
+document.querySelector(".pages").classList.toggle('menuactive');
+document.body.classList.toggle('menuactive');
+document.querySelector(".pages").setAttribute("style","transition: transform 700ms ease-in-out 0s;transform:none!important;")
 });
+menu.addEventListener('touchend', () => {
+  
+  
+  document.querySelector(".pages").classList.toggle('menuactive');
+  document.body.classList.toggle('menuactive');
+  document.querySelector(".pages").setAttribute("style","transition: transform 700ms ease-in-out 0s;transform:none!important;")
+  });
 
 const modalbuttons = document.querySelectorAll(".modalbuttons button");
 const modalpages = document.querySelector(".modalpages > div")
-modalbuttons[0].onclick = ()=>{
+modalbuttons[0].addEventListener("click",()=>{
   if(!modalbuttons[0].classList.contains("active")){
     
-    modalpages.style.transform = "translateX(0)"
+    modalpages.style.transform = "translateX(15px)"
     
     modalbuttons[0].classList.add("active")
     
     modalbuttons[1].classList.remove("active")
   }
-}
-modalbuttons[1].onclick = ()=>{
+})
+modalbuttons[1].addEventListener("click",()=>{
   if(!modalbuttons[1].classList.contains("active")){
     
-    modalpages.style.transform = "translateX(calc(-50% + 30px))"
+    modalpages.style.transform = "translateX(calc(-50% + 75px))"
     
     modalbuttons[1].classList.add("active")
     
     modalbuttons[0].classList.remove("active")
   }
-}
+})
+modalbuttons[0].addEventListener("touchend",()=>{
+  if(!modalbuttons[0].classList.contains("active")){
+    
+    modalpages.style.transform = "translateX(15px)"
+    
+    modalbuttons[0].classList.add("active")
+    
+    modalbuttons[1].classList.remove("active")
+  }
+})
+modalbuttons[1].addEventListener("touchend",()=>{
+  if(!modalbuttons[1].classList.contains("active")){
+    
+    modalpages.style.transform = "translateX(calc(-50% + 75px))"
+    
+    modalbuttons[1].classList.add("active")
+    
+    modalbuttons[0].classList.remove("active")
+  }
+})
+
+const inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
+
+inputs.forEach(elem => {
+ elem.addEventListener("touchend", function(event){
+    elem.focus()
+});
+})
